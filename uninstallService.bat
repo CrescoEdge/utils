05d71e5194435083ab4b6a9bd417886ce5dbbe78
V. K. Cody Bumgardner <cody@uky.edu>
2021-10-14 11:41:16 -0400

    Update uninstallService.bat

--- uninstallService.bat
+++ uninstallService.bat
@@ -1,4 +1,10 @@
 :: Cresco Windows Service Uninstallation
 @echo off
 rem Uninstall service
-prunsrv //DS/CrescoAgent
+
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
+
+if %OS%==32BIT set VERSION=x32
+if %OS%==64BIT set VERSION=x64
+
+%VERSION%\prunsrv //DS/CrescoAgent
